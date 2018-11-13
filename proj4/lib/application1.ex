31bff17df18676326e1d5ff79125189180717976
siddheshgupte <siddhesh.gupte2468@gmail.com>
2018-11-13 09:49:16 +0530

    Add Verify function and change timestamp format

--- proj4/lib/application1.ex
+++ proj4/lib/application1.ex
@@ -1,7 +1,81 @@
 defmodule Application1 do
-    use Application
+  use Application
 
-      def start(_type, num_of_nodes, num_of_messages) do
+  def start(_type, num_of_nodes) do
+    genesis_block = start_blockchain()
+
+    children =
+      1..num_of_nodes
+      |> Enum.to_list()
+      |> Enum.map(fn x ->
+        # Change later if slow
+        identifier = :crypto.hash(:sha, Integer.to_string(x)) |> Base.encode16()
+
+        Supervisor.child_spec(
+          {Proj4, [String.to_atom("#{identifier}"), genesis_block]},
+          id: String.to_atom("#{identifier}")
+        )
+      end)
+
+    opts = [strategy: :one_for_one, name: Supervisor]
+    {:ok, supervisor} = Supervisor.start_link(children, opts)
+
+    Supervisor.which_children(supervisor)
+  end
+
+  def start_blockchain() do
+    [
+      %{
+        :index => 1,
+        :nonce => 2,
+        # :coinbase => [],
+        # :difficulty => ?,
+        :tx => "abc",
+        :prev_hash => "00_000_000_000_000",
+        :time_stamp => 1_542_078_479,
+        :difficulty => 0,
+        :hash => "C16BE0090E95E0FDA0BD22F95E9D5B4B9B1331EE"
+      }
+    ]
+  end
+
+  def find_nonce_and_hash(index, tx, prev_hash, time_stamp, nonce) do
+    ip =
+      Integer.to_string(index) <>
+        tx <> prev_hash <> Integer.to_string(nonce) <> Integer.to_string(time_stamp)
+
+    hex_hash = :crypto.hash(:sha, ip) |> Base.encode16()
+    {int_hash, _} = hex_hash |> Integer.parse(16)
+
+    {nonce, hex_hash} =
+      if rem(int_hash, 2) == 0 do
+        {nonce, hex_hash}
+      else
+        find_nonce_and_hash(index, tx, prev_hash, time_stamp, nonce + 1)
       end
+  end
 
+  # def test_add_transaction(transaction, chain) when is_binary(transaction) do
+  #   prev_block = Enum.at(chain, 0)
+
+  #   # Initializations
+  #   curr_index = prev_block.index + 1
+  #   curr_tx = transaction
+  #   curr_prev_hash = prev_block.hash
+  #   {curr_nonce, curr_hash} = find_nonce(curr_index, curr_tx, curr_prev_hash, 0)
+
+  #   # Make block
+  #   curr_block = 
+  #   %{
+  #     :index => curr_index,
+  #     :nonce => curr_nonce,
+  #     # :coinbase => [],
+  #     :tx => curr_tx,
+  #     :prev_hash => curr_prev_hash,
+  #     :hash => curr_hash,
+  #   }
+
+  #   # Add block to blockchain
+  #   chain = [curr_block | chain]
+  # end
 end
--- proj4/lib/proj4.ex
+++ proj4/lib/proj4.ex
@@ -2,18 +2,133 @@ defmodule Proj4 do
   use GenServer, restart: :temporary
 
   # External API
-  def start_link([input]) do
+  def start_link([input_name, genesis_block]) do
     GenServer.start_link(
       __MODULE__,
       %{
+        :public_key => input_name,
+        :chain => genesis_block,
+        :uncommitted_transactions => []
       },
       name: input_name
     )
   end
 
-   # Genserver Implementation
+  # Genserver Implementation
   def init(initial_map) do
     {:ok, initial_map}
+  end
+
+  # Add a transaction to the front of the local uncommitted transaction list
+  def handle_cast({:add_transaction, transaction}, current_map) when is_binary(transaction) do
+    {_, current_map} =
+      Map.get_and_update(current_map, :uncommitted_transactions, fn x ->
+        {x, [transaction | x]}
+      end)
+
+    {:noreply, current_map}
+  end
+
+  def handle_cast({:mine, transaction}, current_map) when is_binary(transaction) do
+    # Implement consensus
+    # Make every node send {self.pid, chain_length (last_block.index)}
+    # Compare all with own chain length
+    # Make GenServer call to longest chain node
+    # Set highest as current_chain
+
+    # prev_block = Enum.at(current_chain, 0)
+    prev_block = get_last_block(current_map.chain)
+
+    # Initializations
+    curr_index = prev_block.index + 1
+    curr_tx = transaction
+    curr_prev_hash = prev_block.hash
+    curr_timestamp = System.system_time(:second)
+
+    {curr_nonce, curr_hash} =
+      find_nonce_and_hash(curr_index, curr_tx, curr_prev_hash, curr_timestamp, 0)
+
+    # Make block
+    curr_block = %{
+      :index => curr_index,
+      :nonce => curr_nonce,
+      # :coinbase => [],
+      :difficulty => 0,
+      :tx => curr_tx,
+      :prev_hash => curr_prev_hash,
+      :time_stamp => curr_timestamp,
+      :hash => curr_hash
+    }
+
+    # Add block to local chain
+    {_, current_map} = Map.get_and_update(current_map, :chain, fn x -> {x, [curr_block | x]} end)
+    {:noreply, current_map}
+
+    # TODO: send the updated chain to all nodes in the system. 
+  end
+
+  # Print state for debugging
+  def handle_cast({:print_state}, current_map) do
+    IO.inspect(current_map)
+    {:noreply, current_map}
+  end
+
+  def handle_cast({:verify}, current_map) do
+    verify_blockchain(current_map.chain)
+    {:noreply, current_map}
+  end
+
+  # Return Nonce and hexadecimal hash
+  defp find_nonce_and_hash(index, tx, prev_hash, time_stamp, nonce) do
+    ip =
+      Integer.to_string(index) <>
+        tx <> prev_hash <> Integer.to_string(nonce) <> Integer.to_string(time_stamp)
+
+    hex_hash = :crypto.hash(:sha, ip) |> Base.encode16()
+    {int_hash, _} = hex_hash |> Integer.parse(16)
+
+    {nonce, hex_hash} =
+      if rem(int_hash, 2) == 0 do
+        {nonce, hex_hash}
+      else
+        find_nonce_and_hash(index, tx, prev_hash, time_stamp, nonce + 1)
+      end
+  end
+
+  # Return hexadecimal hash
+  defp get_hash(index, tx, prev_hash, time_stamp, nonce) do
+    ip =
+      Integer.to_string(index) <>
+        tx <> prev_hash <> Integer.to_string(nonce) <> Integer.to_string(time_stamp)
+
+    hex_hash = :crypto.hash(:sha, ip) |> Base.encode16()
+  end
+
+  # Return last block of the blockchain
+  defp get_last_block(chain) do
+    Enum.at(chain, 0)
+  end
+
+  # Verify if blockchain is valid
+  defp verify_blockchain(chain) do
+    # Verify linked list
+    invalid_chain =
+      0..(length(chain) - 2)
+      |> Enum.to_list()
+      |> Enum.map(fn x ->
+        if Enum.at(chain, x).prev_hash != Enum.at(chain, x + 1).hash, do: x, else: nil
+      end)
+      |> Enum.filter(fn x -> x != nil end)
+
+    # Verify individual hashes
+    invalid_hash_blocks =
+      Enum.filter(chain, fn x ->
+        x.hash != get_hash(x.index, x.tx, x.prev_hash, x.time_stamp, x.nonce)
+      end)
+
+    IO.inspect(length(invalid_hash_blocks) == 0 and length(invalid_chain) == 0)
+
+    length(invalid_hash_blocks) == 0 and length(invalid_chain) == 0
   end
 
   # CAST EXAMPLE 
@@ -21,7 +136,6 @@ defmodule Proj4 do
   #   IO.inspect(current_map)
   #   {:noreply, current_map}
   # end
-
 
   # CALL EXAMPLE
   # def handle_call(:get_predecessor, _from, current_map) do
@@ -41,5 +155,4 @@ defmodule Proj4 do
   #     Process.send_after(self(), :fix_finger_table, 10_000)
   #   # end
   # end
-
 end
